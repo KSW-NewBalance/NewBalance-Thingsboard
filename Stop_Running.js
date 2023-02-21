4d3ccb895cefc62386618d0eca8defc80820a96d
Hyejin Kim <62338783+WZNT-KimHyeJin@users.noreply.github.com>
2023-02-21 19:03:35 +0900

    Update Stop_Running.js

--- Stop_Running.js
+++ Stop_Running.js
@@ -10,3 +10,27 @@ function getFootAngleAvg(msg, metadata, msgType){
   return {msg: msg, metadata: metadata, msgType: msgType};
 }
 
+
+
+/* setMsgForAttributes(total_fsr_1st,total_fsr_2nd,total_fsr_3rd,total_fsr_4th,avg_foot_angle )
+ * set message to save for shared attribute 
+ * @param total_fsr_1st,total_fsr_2nd,total_fsr_3rd,total_fsr_4th,avg_foot_angle
+ * @return void
+*/ 
+
+function setMsgForAttributes(msg, metadata, msgType){
+
+  var newMsg ={};
+  var newMsgType = "POST_ATTRIBUTES_REQUEST";
+
+  newMsg.total_fsr_1st = metadata.total_fsr_1st;
+  newMsg.total_fsr_2nd = metadata.total_fsr_2nd;
+  newMsg.total_fsr_3rd = metadata.total_fsr_3rd;
+  newMsg.total_fsr_4th = metadata.total_fsr_4th;
+
+  newMsg.avg_foot_angle = msg.avg_foot_angle;
+
+  return {msg: newMsg, metadata: metadata, msgType: newMsgType};
+}
+
+
